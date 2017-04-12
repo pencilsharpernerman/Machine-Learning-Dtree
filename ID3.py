42d042d5dc38c0a7d7ec1edb8ad7442ea8f9d63e
Prithvi S S <prithvisrinivasasan-swachandam2017@u.northwestern.edu>
2017-04-12 16:37:29 -0500

    ID3 helper with attributes

--- ID3.py
+++ ID3.py
@@ -1,158 +1,202 @@
 from node import Node
 import math
-from parse import *
-import Queue as queue
-from sets import Set
-import operator
+from parse import * 
 
+
+
+def uniqueAttributeValues(examples,attribute):
+  attribValues = set()
+  attribValues.add(examples[0][attribute])
+  size = len(examples)
+  for i in range(1,size):
+    if attribValues != examples[i][attribute]:
+      attribValues.add(examples[i][attribute])
+    attributeValues = list(attribValues)
+  return attributeValues
+
+filename = "house_votes_84.data"
+examples = parse(filename)
 classAttributeValues = uniqueAttributeValues(examples,'Class')
 sizeClassAttributeValues = len(classAttributeValues)
-usedAttributes = {'Class' :  True}
 
 
-def ID3(examples, default):
-	totalDataSetSize = len(examples)
-	root = Node()
-	if examples.empty() :
-		return root.set_label(default)
-	elif isSameClassifiication(examples):
-		return root.set_label(examples[0]['Class'])
-	elif  attributes.empty():
-		return root.set_label(calculateMode(examples))
-	else:
-		best = chooseBestAttribute(examples)
-		root.set_label(best['label'])
-		for variable in best['values']:
-			selected = selectExamples(examples,best['label'] , variable)	
-			subtree = ID3(selected,calculateMode(selected))
-			root.set_children(variable,subtree)
-	return root
-		 
-def uniqueAttributeValues(examples,attribute):
-	attribValues = set()
-	attribValues.add(examples[0][attribute])
-	size = len(examples)
-	for i in range(1,size):
-		if attribValues != examples[i][attribute]:
-			attribValues.add(examples[i][attribute])
-	attributeValues = list(attribValues)
-	return attributeValues
+def isSameClassification(examples):
+  sampleElement = examples[0]['Class']
+  for item in examples[1:]:
+    if item != sampleElement:
+      return False
+  return True
 
+def getAttributes(examples):
+  list_attr = []
+  for dic in examples[1]:
+    if dic != "Class":
+      list_attr.append(dic)
+  return list_attr
 
- def isSameClassification(examples):
- 	sampleElement = examples[0]['Class']
- 	for item in examples[1:]
- 		if item != sampleElement
- 			return false
-	return true
-
-
- def calculateMode(examples):
-	count = list()
- 	size_examples = len(examples)
- 	size_classAttrib = len(classAttributeValues)
-	for i in range (0, size_examples) :
-		for j in range(0,size_classAttrib):
-			if  examples[i]['Class'] == classAttributeValues[j] 
- 				count[j] += 1
- 	max  = 0 
- 	for i in range(0,size_classAtrrib)
- 		if count[i] >= max
- 			max = i 
- 	return classAttributeValues[max]
+def calculateMode(examples):
+  count = [0]
+  size_examples = len(examples)
+  size_classAttrib = len(classAttributeValues)
+  count=count*2
+  for i in range (0, size_examples) :
+    for j in range(0,size_classAttrib):
+      if  examples[i]['Class'] == classAttributeValues[j]: 
+        count[j] += 1
+  max  = 0 
+  for i in range(0,size_classAttrib):
+    if count[i] >= max:
+      max = i 
+  return classAttributeValues[max]
 
 def Hprior(examples,attribute='Class'):
-	count = dict.fromkeys(classAttributeValues,0)
-	size = len(examples)
-	for i in range (0, size) :
-		for j in range (0,sizeClassAttributeValues):
-	 		if  examples[i][attribute] == classAttributeValues[j]: 
-				count[classAttributeValues[j]] += 1  				
+  count = dict.fromkeys(classAttributeValues,0)
+  size = len(examples)
+  for i in range (0, size) :
+    for j in range (0,sizeClassAttributeValues):
+      if  examples[i][attribute] == classAttributeValues[j]: 
+        count[classAttributeValues[j]] += 1         
 
- return entropyWrapper(count)
+  return entropyWrapper(count)
 
 
 
 def entropyWrapper(count):
-	if len(count) is not 0:
-	totalSum = 0
-	hPrior = 0 
-	for i in range(0,len(count)):
-		totalSum += count[i]   
-	for  i in range(0,len(count)):
-		hprior -= count[i]/totalSum * math.log(count[i]/totalSum,2)
-return 	hPrior
-
- def entropy(examples,attribute):
- 	attributeValuesList = list()
-	attributeValues = uniqueAttributeValues(examples, attribute)
-	sizeAttribValues = len(attributeValues)
-	if attribute != None:
-		for i in range(0, sizeAttribValues):
-			attributeValuesList[i] = dict.fromkeys(classAttributeValues, 0)
-		for instance in examples:
-			for i in range(0,sizeAttribValues):
-				if instance[attribute] == attributeValues[i]:
-					attributeValuesList[i] = entropyLoop(instance,attribute,dict_i)
-return attributeValuesList				
+  if len(count) is not 0:
+    totalSum = 0.0
+    hPrior = 0.0 
+    for key in count:
+      totalSum += count[key]   
+    for key in count:
+      hPrior -= count[key]/totalSum * math.log(count[key]/totalSum,2.0)
+  return  hPrior
 
 
-
-def entropyLoop(examples,attribute,dict_i):
-
-	for j in range(0,sizeClassAttributeValues):
-		if instance['Class'] == classAttributeValues[j]:
-			attributeValuesList[i][classAttributeValues[j]] += 1
-			break				 
-return attributeValuesList[i]
+def entropy(examples,attribute):
+  attributeValuesList = [0]
+  attributeValues = uniqueAttributeValues(examples, attribute)
+  #print attributeValues
+  sizeAttribValues = len(attributeValues)
+  attributeValuesList*=sizeAttribValues
+  if attribute != None:
+    for i in range(0, sizeAttribValues):
+      attributeValuesList[i] = dict.fromkeys(classAttributeValues, 0)
+    #print attributeValuesList
+    for instance in examples:
+      for i in range(0,sizeAttribValues):
+        if instance[attribute] == attributeValues[i]:
+          for j in range(0,sizeClassAttributeValues):
+            if instance['Class'] == classAttributeValues[j]:
+              attributeValuesList[i][classAttributeValues[j]] += 1
+             
+  return attributeValuesList        
 
 def proportion(dictionary,examples):
-    if len(examples) is not 0:
+  totalSum=0.0
+  if len(examples) is not 0:
     totalDataSetSize = len(examples)
-    
-    for i in range(0,len(count)):
-        totalSum += count[i]
+  
+  for key in dictionary:
+    totalSum += dictionary[key]
 
-    return (totalSum/totalDataSetSize)
+  return (totalSum/totalDataSetSize)
 
 def calculateInfoGain(examples,attribute):
-	hprior = Hprior(examples)
-    informationGain = {attribute:None}
-    entropyList = list()
-    totalSum = len(examples)
-    attributeValuesList = entropy(examples,attribute)
-	for i in range(0,sizeAttributeValues):
-      for j in range(0,sizeClassAttributeValues):
-          entropySum += entropyWrapper(attributeValuesList[i]) * proportion(attributeValuesList[i],examples)    
-      
-    informationGain[attribute] = Hprior(examples) - entropySum
-    return informationGain
-	
+  hprior = Hprior(examples)
+  informationGain = {attribute:None}
+  entropyList = list()
+  entropySum=0
+  #totalSum = len(examples)
+  attributeValuesList = entropy(examples,attribute)
+  sizeAttributeValues = len(attributeValuesList)
+  for i in range(0,sizeAttributeValues):
+    #for j in range(0,sizeClassAttributeValues):
+    entropySum += entropyWrapper(attributeValuesList[i]) * proportion(attributeValuesList[i],examples)    
+    #print entropyWrapper(attributeValuesList[i])  
+    #print proportion(attributeValuesList[i],examples)   
+  informationGain[attribute] = Hprior(examples) - entropySum
+  return informationGain
 
 def getMaxInfoGain(dictionary):
-	v=list(dictionary.values())
-	k=list(dictionary.keys())
-	return k[v.index(max(v))]
+  v=list(dictionary.values())
+  k=list(dictionary.keys())
+  return k[v.index(max(v))]
 
 
 def chooseBestAttribute(examples):
-	result = {'label' : None, 'values': None}
-	informationGain = {}
-	attributes = getAttributes(examples)
-	for attrib in attributes : 
-		if attrib not in usedAttributes:
-			calculateInfoGain(examples,attrib)
-	result = getMaxInfoGain(informationGain)
-	usedAttributes[result] = True
-	attributeValues = uniqueAttributeValues(examples,result)
-	return dict(label=result,values=attributeValues)
+  result = {'label' : None, 'values': None}
+  informationGain = {}
+  attributes = getAttributes(examples)
+  for attrib in attributes : 
+    if attrib not in usedAttributes:
+      informationGain.update(calculateInfoGain(examples,attrib))
+  result = getMaxInfoGain(informationGain)
+  usedAttributes[result] = True
+  attributeValues = uniqueAttributeValues(examples,result)
+  return dict(label=result,values=attributeValues)
+
 
 def selectExamples(examples,attribute,variable):
-	subExamples= []
-	for instance in examples:
-		if instance[attribute] is variable:
-			subExamples.append(instance)
-	return subExamples
+  subExamples= []
+  for instance in examples:
+    if instance[attribute] is variable:
+      subExamples.append(instance)
+  return subExamples
+
+def ID3(examples, default):
+  attributes = getAttributes(examples)
+  ID3(examples, attributes,default)
+  
+
+
+def ID3(examples,attributes,default):
+
+  totalDataSetSize = len(examples)
+  root = Node()
+  if len(examples)==0 :
+    return root.set_label(default)
+  elif isSameClassification(examples):
+    return root.set_label(examples[0]['Class'])
+  elif  len(attributes)==0:
+    return root.set_label(calculateMode(examples))
+  else:
+    best = chooseBestAttribute(examples)
+    root.set_label(best['label'])
+    for variable in best['values']:
+      selected = selectExamples(examples,best['label'] , variable)
+      if best in attributes: 
+          attributes.remove[best]  
+      subtree = ID3(selected, attributes ,calculateMode(selected))
+      root.set_children(variable,subtree)
+  return root
+
+  '''
+  Takes in an array of examples, and returns a tree (an instance of Node) 
+  trained on the examples.  Each example is a dictionary of attribute:value pairs,
+  and the target class variable is a special attribute with the name "Class".
+  Any missing attributes are denoted with a value of "?"
+  '''
+def breadth_first_search(root):
+  ans=""
+  if root.get_value()==None:
+    ans="Tree is empty"
+  else:
+    q = Queue()
+    q.put(root)
+    while True:
+      if q.empty():
+        break
+      a=q.get()
+      ans=ans+str(a.get_value())+" "
+      if(a.get_children()!=None):
+        children=a.get_children()
+        noofchilderen=len(children)
+        for i in range(1,noofchilderen+1):
+          q.put(children[i])
+  return ans[0:len(ans)-1]
+
+
+
 
 def prune(node, examples):
   '''
@@ -174,35 +218,17 @@ def evaluate(node, example):
   '''
 
 
+#if __name__ == '__main__':
 
-
-if __name__ == '__main__':
-	filename = "house_votes_84.data"
-	examples = parse(filename)
-	
-	# sampleElement = examples[0][attrib]
-	# print sampleElement
-	
-	#print classAttributeValues
-	
-	#entropy(examples, attribute)
-
-	# if attribute != None:
-	# 	for i in range(0, sizeAttribValues):
-	# 		attributeValuesList[i] = dict.fromkeys(classAttributeValues, 0)
-	# 	for instance in examples:
-	# 		for i in range(0,sizeAttribValues):
-	# 			if instance[attribute] == attributeValues[i]:
-	# 				attributeValuesList[i][attributeValues[i]] += 1
-	# 				print attributeValues[i]
-	# attribute = 'handicapped-infants'
-	# attributeValues = uniqueAttributeValues(examples, attribute)
-	# #print attributeValues[0]
-
-	# sizeAttribValues = len(attributeValues)
-	
-							#print dict_i[classAttributeValues[j]] 
-	# for instance in examples:					
-	# 	for i in range(0, sizeClassAttributeValues):
-	# 		print instance['Class']
-	
+  #print classAttributeValues
+  #print sizeClassAttributeValues
+  #print usedAttributes
+  #print isSameClassification(examples)
+  #print calculateMode(examples)
+  #print Hprior(examples)
+  #print entropy(examples,'physician-fee-freeze')
+  #print calculateInfoGain(examples,'physician-fee-freeze')
+  #print chooseBestAttribute(examples)
+  #print len(selectExamples(examples,'physician-fee-freeze','y'))
+  #print getAttributes(examples)
+  #ID3(examples,0)
